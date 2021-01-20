5b50022e71244f746e2bf146404a2afc59a3dc0a
Filip Tepper <filip@tepper.pl>
2021-01-20 20:51:00 +0100

    initial commit

---
+++ exec/wrapper-terraform-fmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash +e
+
+terraform fmt -recursive -check > _TERRAFORM_LIST
+
+while IFS= read -r FILE; do
+  DIFF=$(terraform fmt -check -diff "$FILE")
+
+  echo "diff $DIFF" >> _TERRAFORM_DIFF
+done < _TERRAFORM_LIST
+
+reviewdog -f=diff -reporter=github-pr-review < _TERRAFORM_DIFF
